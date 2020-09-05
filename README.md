d135fba575fb2a2ff1194972fed679bffdece84f
ZedDev <43217974+zedd3v@users.noreply.github.com>
2020-09-05 15:15:52 +0200

    updated to 1.64

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 Abck is a library that makes generating akamai cookies easier.
 
-Currently does not support all of the functions/methods. Expect much more in an upcoming update.
+Supported browser: chrome, firefox.
+Please contribute values for other browsers.
 
 
 ## Installation
@@ -10,49 +11,29 @@ Currently does not support all of the functions/methods. Expect much more in an 
 ```bash
 $ npm install abck
 ```
+```bash
+$ yarn add abck
+```
 
 ## Usage
 
-```javascript
-const Abck = require('abck').default;
+```typescript
+import Abck from 'abck'; // const Abck = require('abck');
 
-// Typescript: import Abck from 'abck';
-
-Abck.validateCookie(['=='], "C0E79E904291F0101B699F08B90AE09C~-1~YAAQvnEGF1n4o7ByAQAASRpJwAQ+zePjLXV6UyhVffBSzV8Rjd2nU3orgVbMnYvAao6Xgw8Feycm9b55c0HQgOWFePtq2y1Cc/f+FhPcJQYsSgeFZz1Rs/pBhI6Db/aCcjJd9WFJxZ5G+LOsuFezyoJDmY2FK4BAfTYKQ1oPqKN36Rjmqm6b+KMn8wq2/4rukChJhuFD4b/UDEcqCAdMguRAlosZrDg5y1GTsiMnjOPzxSJLoqsPT9tVf2tjVJw94fLQcyNrYYSPDvliWmI4WDzg6zRpkjBjfmsiXnwtApPKDSoahrM3ykGSOdHAwJGuInCZcrDayC7VwXlPPYYAzO9nVP95qnaVAA==~-1~-1~-1");
-// => true
-
-const d3 = Abck.d3();
-
-Abck.gd(Date.now(), d3, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36", {
-	availWidth: 1680,
-	availHeight: 972,
-	width: 1680,
-	height: 1050,
-	innerWidth: 1680,
-	innerHeight: 939,
-	outerWidth: 1680,
-});
-// => Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36,uaend,12147,0030107,en-US,Gecko,3,0,0,0,391828,9560794,1680,972,1680,1050,1680,939,1680,,cpen:0,i1:0,dm:0,cwen:0,non:1,opc:0,fc:0,sc:0,wrc:1,isc:0,vib:1,bat:1,x11:0,x12:1,8276,0.744979299372,796244780392,loc:
-
-Abck.o9(d3);
-
-// => 914527596
-
-Abck.sed();
-// => "0,0,0,0,1,0,0"
-
-Abck.ab("test");
-// => 448
-
-Abck.np();
-// => "10321144241322243122"
-
-Abck.getmr(); //Needs to be executed inside an electron window
-// => "58,60,289,59,113,56,26,21,25,9,9,9,20,588,"
-
-Abck.od("0a46G5m17Vrp4o4c", "afSbep8yjnZUjq3aL010jO15Sawj2VZfdYK8uY90uxq");
-// => "7a74G7m23Vrp0o5c"
-
+function validateCookie(cookieChecks: string[], abck: string): boolean
+function d3(): number
+function od(t: string, a: string): string
+function ab(t: string): number
+function uar(userAgent: string): string
+function fas(browser: Browser): number
+function sed(): string
+function np(browser: Browser): string
+function jrs(t: number): number[]
+function o9(d3: number): number
+function getType(t: string): number
+function z1(startTimestamp: number): number
+function gd(browser: Browser, browserData: BrowserData, startTimestamp: number, d3: number): string
+function getmr(): string // Only works correctly in browser
 ```
 
 ## Maintainer
--- src/index.ts
+++ src/index.ts
@@ -1,171 +1,317 @@
 import { performance } from 'perf_hooks';
 
-interface CustomScreen {
-    availWidth: number;
-    availHeight: number;
-    width: number;
-    height: number;
-    innerWidth: number;
-    innerHeight: number;
-    outerWidth: number;
+export type Browser = 'chrome' | 'firefox' | 'brave' | 'safari' | 'ie' | 'edge';
+
+export interface ScreenData {
+  availWidth: number;
+  availHeight: number;
+  width: number;
+  height: number;
+  innerWidth: number;
+  innerHeight: number;
+  outerWidth: number;
 }
 
-// interface WebsiteObject {
-// 	url: string;
-// 	/**
-// 	 * Headers used on all requests (initialAbck, formInfo...)
-// 	 */
-// 	// eslint-disable-next-line @typescript-eslint/ban-types
-// 	headers: object;
-// 	/**
-// 	 * Optional custom url to fetch form info from.
-// 	 */
-// 	getFormInfoUrl?: string;
-// }
+export interface BrowserData {
+  lang: string;
+  userAgent: string;
+  screenData: ScreenData;
+}
+
+const UNSUPPORTED_BROWSER_ERROR = new Error('Browser not fully supported. Please contribute.');
+
+const randomRange = (min: number, max: number): number => ~~(Math.random() * (max - min + 1)) + min;
 
 class Abck {
-	/**
-     * Validates cookie
-     * @param cookieChecks Append an ! (exclamation mark) for negative outlook. E.g. ["!==", "~-1"]
-     * @param abck 
-     * @returns boolean
-     */
-    static validateCookie(cookieChecks: string[], abck: string): boolean {
-        return cookieChecks.every(s => s.startsWith("!") ? !abck.includes(s.substr(1)) : abck.includes(s));
-    }
+  /**
+   * @param cookieChecks Append an ! (exclamation mark) for negative outlook. E.g. ["==", "~-1", "!-1"]
+   */
+  static validateCookie(cookieChecks: string[], abck: string): boolean {
+    return cookieChecks.every((s) =>
+      s.startsWith('!') ? !abck.includes(s.substr(1)) : abck.includes(s),
+    );
+  }
 
-    static sed(): string {
-        return "0,0,0,0,1,0,0";
-    }
+  static d3(): number {
+    return Date.now() % 1e7;
+  }
 
-    private static uar(userAgent: string): string {
-        return userAgent.replace(/\\|"/g, "");
-    }
+  private static rir(t: number, a: number, e: number, n: number): number {
+    return t > a && t <= e && (t += n % (e - a)) > e && (t = t - e + a), t;
+  }
 
-    static ab(t: string): number {
-        if (t == null) return "-1";
-        try {
-            let a = 0;
-            for (let e = 0; e < t.length; e++) {
-                const n = t.charCodeAt(e);
-                n < 128 && (a += n)
-            }
-            return a;
-        } catch (e) {
-            return -2;
+  static od(t: string, a: string): string {
+    try {
+      t = String(t);
+      a = String(a);
+      let e = [];
+      const n = a.length;
+      if (n > 0) {
+        for (var o = 0; o < t.length; o++) {
+          let m = t.charCodeAt(o);
+          let r = t.charAt(o);
+          const i = a.charCodeAt(o % n);
+          m = this.rir(m, 47, 57, i);
+          m != t.charCodeAt(o) && (r = String.fromCharCode(m));
+          e.push(r);
         }
-    }
+        if (e.length > 0) return e.join('');
+      }
+    } catch (t) {}
+    return t;
+  }
 
-    static np(): string {
-        return "10321144241322243122";
+  static ab(t: string): number {
+    if (null == t) return -1;
+    try {
+      let a = 0;
+      for (let e = 0; e < t.length; e++) {
+        const n = t.charCodeAt(e);
+        n < 128 && (a += n);
+      }
+      return a;
+    } catch (t) {
+      return -2;
     }
+  }
 
-	/**
-	 * Needs to be executed inside electron app (returns weird results on node)
-	 * @returns getmr 
-	 */
-    static getmr(): string {
-        const e = [Math.abs, Math.acos, Math.asin, Math.atanh, Math.cbrt, Math.exp, Math.random, Math.round, Math.sqrt, isFinite, isNaN, parseFloat, parseInt, JSON.parse];
-        let t = "";
-        for (let n = 0; n < e.length; n++) {
-            const o = [];
-            let m = 0;
-            const r = performance.now();
-            let i = 0;
-            let c = 0;
-            if (void 0 !== e[n]) {
-                for (i = 0; i < 1000 && m < .6; i++) {
-                    const b = performance.now()
-                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                    // @ts-ignore
-                    for (let d = 0; d < 4e3; d++) e[n](3.14);
-                    const k = performance.now();
-                    o.push(Math.round(1e3 * (k - b))), m = k - r
-                }
-                const s = o.sort();
-                c = s[Math.floor(s.length / 2)] / 5
-            }
-            t = `${t}${c},`
+  static uar(userAgent: string): string {
+    return userAgent.replace(/\\|"/g, '');
+  }
+
+  static fas(browser: Browser): number {
+    switch (browser) {
+      case 'chrome':
+        return 30261693;
+      case 'firefox':
+        return 26067385;
+      case 'brave':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      case 'safari':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      case 'ie':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      case 'edge':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      default:
+        throw UNSUPPORTED_BROWSER_ERROR;
+    }
+  }
+
+  static sed(): string {
+    return '0,0,0,0,1,0,0';
+  }
+
+  static np(browser: Browser): string {
+    switch (browser) {
+      case 'chrome':
+        return '10321144241322243122';
+      case 'firefox':
+        return '11133333331333333333';
+      case 'brave':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      case 'safari':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      case 'ie':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      case 'edge':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      default:
+        throw UNSUPPORTED_BROWSER_ERROR;
+    }
+  }
+
+  private static cal_dis(t: number[]): number {
+    var a = t[0] - t[1],
+      e = t[2] - t[3],
+      n = t[4] - t[5],
+      o = Math.sqrt(a * a + e * e + n * n);
+    return Math.floor(o);
+  }
+
+  static jrs(t: number): number[] {
+    let a = Math.floor(1e5 * Math.random() + 1e4);
+    let o = [];
+    for (let e = String(t * a), n = 0, m = e.length >= 18; o.length < 6; ) {
+      o.push(parseInt(e.slice(n, n + 2))), (n = m ? n + 3 : n + 2);
+    }
+    return [a, this.cal_dis(o)];
+  }
+
+  private static cc(t: number) {
+    let a = t % 4;
+    2 == a && (a = 3);
+    const e = 42 + a;
+    let n = (_t: number, _a: number) => 0;
+    if (42 == e) {
+      n = (t, a) => t * a;
+    } else if (43 == e) {
+      n = (t, a) => t + a;
+    } else {
+      n = (t, a) => t - a;
+    }
+    return n;
+  }
+
+  static o9(d3: number): number {
+    let a = d3;
+    for (let n = 0; n < 5; n++) {
+      const o = Math.floor(d3 / Math.pow(10, n)) % 10;
+      const m = o + 1;
+      const op = this.cc(o);
+      a = op(a, m);
+    }
+    return a * 3;
+  }
+
+  static getType(t: string): number {
+    t = t.toLowerCase();
+    return 'text' == t || 'search' == t || 'url' == t || 'email' == t || 'tel' == t || 'number' == t
+      ? 0
+      : 'password' == t
+      ? 1
+      : 2;
+  }
+
+  static z1(startTimestamp: number): number {
+    return Math.floor(startTimestamp / (2016 * 2016));
+  }
+
+  static gd(
+    browser: Browser,
+    browserData: BrowserData,
+    startTimestamp: number,
+    d3: number,
+  ): string {
+    const userAgent = this.uar(browserData.userAgent);
+    const randomNumber = Math.random();
+    let xagg: number;
+    let psub: number;
+    let plen: number;
+    let bd: string;
+
+    switch (browser) {
+      case 'chrome':
+        xagg = 12147;
+        psub = 20030107;
+        plen = 3;
+        bd = ',cpen:0,i1:0,dm:0,cwen:0,non:1,opc:0,fc:0,sc:0,wrc:1,isc:0,vib:1,bat:1,x11:0,x12:1';
+        break;
+      case 'firefox':
+        xagg = 11059;
+        psub = 20100101;
+        plen = 0;
+        bd = `,cpen:0,i1:0,dm:0,cwen:0,non:1,opc:0,fc:1,sc:0,wrc:1,isc:${randomRange(
+          60,
+          100,
+        )},vib:1,bat:0,x11:0,x12:1`;
+        break;
+      case 'brave':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      case 'safari':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      case 'ie':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      case 'edge':
+        throw UNSUPPORTED_BROWSER_ERROR;
+      default:
+        throw UNSUPPORTED_BROWSER_ERROR;
+    }
+    return (
+      userAgent +
+      ',' +
+      'uaend' +
+      ',' +
+      xagg +
+      ',' +
+      psub +
+      ',' +
+      browserData.lang +
+      ',' +
+      'Gecko' +
+      ',' +
+      plen +
+      '0,0,0' +
+      ',' +
+      this.z1(startTimestamp) +
+      ',' +
+      d3 +
+      ',' +
+      browserData.screenData.availWidth +
+      ',' +
+      browserData.screenData.availHeight +
+      ',' +
+      browserData.screenData.width +
+      ',' +
+      browserData.screenData.height +
+      ',' +
+      browserData.screenData.innerWidth +
+      ',' +
+      browserData.screenData.innerHeight +
+      ',' +
+      browserData.screenData.outerWidth +
+      ',' +
+      bd +
+      ',' +
+      this.ab(userAgent) +
+      ',' +
+      String(randomNumber).slice(0, 11) +
+      String(Math.floor((1000 * randomNumber) / 2)) +
+      ',' +
+      startTimestamp / 2 +
+      ',' +
+      0 +
+      ',' +
+      'loc:'
+    );
+  }
+
+  /**
+   * Needs to be executed inside browser/electron app (returns weird results in node)
+   * @returns getmr
+   */
+  static getmr(): string {
+    const e = [
+      Math.abs,
+      Math.acos,
+      Math.asin,
+      Math.atanh,
+      Math.cbrt,
+      Math.exp,
+      Math.random,
+      Math.round,
+      Math.sqrt,
+      isFinite,
+      isNaN,
+      parseFloat,
+      parseInt,
+      JSON.parse,
+    ];
+    let t = '';
+    for (let n = 0; n < e.length; n++) {
+      const o = [];
+      let m = 0;
+      const r = performance.now();
+      let i = 0;
+      let c = 0;
+      if (void 0 !== e[n]) {
+        for (i = 0; i < 1000 && m < 0.6; i++) {
+          const b = performance.now();
+          // @ts-ignore
+          for (let d = 0; d < 4e3; d++) e[n](3.14);
+          const k = performance.now();
+          o.push(Math.round(1e3 * (k - b))), (m = k - r);
         }
-        return t;
+        const s = o.sort();
+        c = s[Math.floor(s.length / 2)] / 5;
+      }
+      t = `${t}${c},`;
     }
-
-	/**
-     * @param userAgent 
-     * @param startTimestamp Use Date.now()
-     * @param screenData CustomScreen Object
-     * @returns gd value 
-     */
-    static gd(startTimestamp: number, d3: number, userAgent: string, screenData: CustomScreen): string {
-        const t = this.uar(userAgent);
-        const d = Math.random();
-        return `${t},uaend,12147,20030107,en-US,Gecko,3,0,0,0,${parseInt((startTimestamp / 4064256).toString())},${d3},${screenData.availWidth},${screenData.availHeight},${screenData.width},${screenData.height},${screenData.innerWidth},${screenData.innerHeight},${screenData.outerWidth},,cpen:0,i1:0,dm:0,cwen:0,non:1,opc:0,fc:0,sc:0,wrc:1,isc:0,vib:1,bat:1,x11:0,x12:1,${this.ab(t)},${d.toString().slice(0, 11) + parseInt((1000 * d / 2).toString())},${startTimestamp / 2},loc:`;
-    }
-
-    static d3(): number {
-        return Date.now() % 1e7;
-    }
-
-    static od(t: string, a: string): string {
-        try {
-            t = String(t),
-                a = String(a);
-            const e = [];
-            if (a.length > 0) {
-                for (let o = 0; o < t.length; o++) {
-                    let m = t.charCodeAt(o);
-                    let r = t.charAt(o);
-                    const i = a.charCodeAt(o % a.length);
-                    m = this.rir(m, 47, 57, i);
-                    m != t.charCodeAt(o) && (r = String.fromCharCode(m));
-                    e.push(r);
-                }
-                if (e.length > 0) return e.join("")
-            }
-        } catch (t) {
-            throw new Error(t);
-        }
-        return t;
-    }
-
-    private static rir(t: number, a: number, e: number, n: number): number {
-        return t > a && t <= e && (t += n % (e - a)) > e && (t = t - e + a), t;
-    }
-
-    static o9(d3: number): number {
-        let a = d3;
-        for (let n = 0; n < 5; n++) {
-            const o = d3 / Math.pow(10, n) % 10;
-            const m = o + 1;
-            const op = this.cc(o);
-            a = op(a, m);
-        }
-        return a * parseInt(String.fromCharCode(51));
-    }
-
-    private static cc(t: number) {
-        let a = t % 4;
-        2 == a && (a = 3);
-        const e = 42 + a;
-        let n: {
-            (t: number, a: number): number;
-            (t: number, a: number): number;
-            (t: number, a: number): number;
-        };
-        if (42 == e) {
-            n = function (t: number, a: number) {
-                return t * a
-            }
-        } else if (43 == e) {
-            n = function (t: number, a: number) {
-                return t + a
-            }
-        } else {
-            n = function (t: number, a: number) {
-                return t - a
-            }
-        }
-        return n;
-    }
+    return t;
+  }
 }
 export default Abck;
+
+module.exports = Abck;
+module.exports.default = Abck;
+module.exports.__esModule = true;
